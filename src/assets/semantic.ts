2fd01428c9db01451322ddafb38006d00957e3d3
Marcelo Pignataro <pignataromd@gmail.com>
2024-02-22 10:08:19 +0000

    better project structure

---
+++ src/assets/semantic.ts
@@ -0,0 +1,15 @@
+const primitives = {
+  color: {
+    base: {
+      '500': '#E7C87B',
+    },
+  },
+};
+
+export const semantic = {
+  tag: {
+    bg: {
+      primary: primitives.color.base[500],
+    },
+  },
+};
---
+++ src/types/components/Tag.d.ts
@@ -0,0 +1,7 @@
+export type tagType =
+  | 'disabled'
+  | 'primary'
+  | 'danger'
+  | 'info'
+  | 'success'
+  | 'warning';
