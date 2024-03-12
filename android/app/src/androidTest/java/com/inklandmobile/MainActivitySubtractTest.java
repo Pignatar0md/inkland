a1c9253658f5791a563cd4a8ac486410d69219b8
Marcelo Pignataro <pignataromd@gmail.com>
2024-03-12 19:07:12 +0000

    ios Icons

--- android/app/src/androidTest/java/com/inklandmobile/MainActivitySubtractTest.java
+++ android/app/src/androidTest/java/com/inklandmobile/MainActivitySubtractTest.java
@@ -4,7 +4,6 @@ package com.inklandmobile;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -22,30 +21,30 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.After;
-import org.junit.runner.RunWith;
-
 import com.microsoft.appcenter.espresso.Factory;
 import com.microsoft.appcenter.espresso.ReportHelper;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+public class MainActivitySubtractTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Rule
-    public  ReportHelper reportHelper = Factory.getReportHelper();
+    public ReportHelper reportHelper = Factory.getReportHelper();
 
     @Test
-    public void mainActivityTest() {
+    public void mainActivitySubtract() {
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
@@ -58,6 +57,7 @@ public class MainActivityTest {
                                         0),
                                 0),
                         isDisplayed()));
+        reactEditText.perform(click());
 
         ViewInteraction reactEditText2 = onView(
                 allOf(childAtPosition(
@@ -66,17 +66,16 @@ public class MainActivityTest {
                                         0),
                                 0),
                         isDisplayed()));
-        reactEditText2.perform(replaceText("3"), closeSoftKeyboard());
+        reactEditText2.perform(click());
 
         ViewInteraction reactEditText3 = onView(
-                allOf(withText("3"),
-                        childAtPosition(
+                allOf(childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
                                 0),
                         isDisplayed()));
-        reactEditText3.perform(pressImeActionButton());
+        reactEditText3.perform(replaceText("4"), closeSoftKeyboard());
 
         ViewInteraction reactEditText4 = onView(
                 allOf(childAtPosition(
@@ -85,6 +84,7 @@ public class MainActivityTest {
                                         0),
                                 1),
                         isDisplayed()));
+        reactEditText4.perform(click());
 
         ViewInteraction reactEditText5 = onView(
                 allOf(childAtPosition(
@@ -93,31 +93,13 @@ public class MainActivityTest {
                                         0),
                                 1),
                         isDisplayed()));
-
-        ViewInteraction reactEditText6 = onView(
-                allOf(childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        reactEditText6.perform(replaceText("2"), closeSoftKeyboard());
-
-        ViewInteraction reactEditText7 = onView(
-                allOf(withText("2"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        reactEditText7.perform(pressImeActionButton());
+        reactEditText5.perform(replaceText("3"), closeSoftKeyboard());
         ViewInteraction button = onView(
-                allOf(withText("SUM"),
+                allOf(withText("QUIT"),
                         isDisplayed()));
-        button.perform(click());
+            button.perform(click());
         ViewInteraction textView = onView(
-                allOf(withText("5"),
+                allOf(withText("1"),
                         withParent(withParent(withId(android.R.id.content))),
                         isDisplayed()));
         try {
@@ -125,14 +107,12 @@ public class MainActivityTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        textView.check(matches(withText("5")));
+        textView.check(matches(withText("1")));
     }
-
     @After
     public void TearDown() {
         reportHelper.label("Stopping App");
     }
-
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
